52a46de0a7afc1f87cf4bb548e44e88df877b7a3
zhangweixu <1689719986@qq.com>
2020-05-05 12:29:13 +0800

    合并分支

--- test.py
+++ test.py
@@ -6,4 +6,24 @@ class Test(unittest.TestCase):
     测试用例
     """
     def test_order(self):
-        """订单"""
+        """订单管理"""
+        # master分支
+
+
+
+
+
+    def test_release(self):
+        """测试分支"""
+        
+
+
+
+
+
+
+
+
+
+
+
